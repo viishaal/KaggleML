28f4967011d6f28c1027c58b3f5115d21ed27c7a
vishal <vj2208@columbia.edu>
2016-04-12 05:05:13 -0400

    find independent columns in a datframe

--- find_independent.py
+++ find_independent.py
@@ -1,10 +1,13 @@
 import numpy as np
 linalg = np.linalg
 from read_data import *
+from sklearn import preprocessing
 
 _TRAINING_FILE_NAME_ = "Data/data.csv"
 _TEST_FILE_NAME_ = "Data/quiz.csv"
 _FIELDS_FILE_ = "Data/field_types.txt"
+
+BLACKLIST = ['18','20','23','25','26','58']
 
 def independent_columns(A, tol = 1e-05):
     """
@@ -38,18 +41,42 @@ if __name__ == "__main__":
     train_labels = train_labels.reshape(train_labels.size, 1)
 
     train_data = data.drop("label", 1)
-    #train_data =train_data.drop(['18','20','23','25','26','58'], axis=1)
+    train_data =train_data.drop(BLACKLIST, axis=1)
 
     ft = open(_FIELDS_FILE_, "r")
     categ = []      # list of categorical variables for transform
     non_categ = []
+    categ = []
     for line in ft.readlines():
         splits = line.split()
-        if splits[1] != "numeric":
+        if splits[1] == "numeric":
+            non_categ.append(splits[0])
+        elif splits[0] not in BLACKLIST:
             categ.append(splits[0])
 
+    train_data =train_data.drop(non_categ, axis=1)
 
-    train_data =train_data.drop(categ, axis=1)
+    # for i,c in enumerate(categ):
+    #     le = preprocessing.LabelEncoder()
+    #     train_data[c] = le.fit_transform(train_data[c])
+
+
+    print train_data.shape
+
+    one_hot_features = []
+    for c in categ:
+        print c
+        lb = preprocessing.LabelBinarizer()
+        mat = lb.fit_transform(train_data[c])
+        print mat[1:10,:]
+        one_hot_features.append(mat)
+
+    train_data = train_data.drop(categ, axis=1)
+
+    for mat in one_hot_features:
+        train_data = pd.concat([train_data, pd.DataFrame(mat)], axis=1)
+
+    print train_data.shape
 
     indx = independent_columns(train_data.as_matrix())
     print len(indx)
